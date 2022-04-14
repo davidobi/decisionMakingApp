0b7b17cf0e32274efdce6b61d3b9b547bec2d043
davidobi <dbiggestdave@yahoo.com>
2022-04-14 22:19:47 +0000

    Editing app file to return title and subtitle

--- decisionmaker/src/App.js
+++ decisionmaker/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+   const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of  computer'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+  </div>
   );
 }
 

ea047a77b3333ecebbf5210aad5420dc74bf3e40
davidobi <dbiggestdave@yahoo.com>
2022-04-15 11:57:05 +0000

    converting to component based

--- decisionmaker/src/App.js
+++ decisionmaker/src/App.js
@@ -1,62 +1,48 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-
-  const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of  computer',
-    options: []
-  };
-
-  const onFormSubmitHandler = (e) => {
-    e.preventDefault();
-    const option = e.target.elements.option.value;
-    if (option) {
-      app.options.push(option);
-      e.target.elements.option.value = '';
-    }
+class Header extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Indecision </h1>
+        <h2>Put your life ib the hands of a computer</h2>
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    )
   }
-
-  const removeAllHandler = () => {
-    app.options = [];
-  }
-  
- const makeDecisionHandler = () => {
-   const randomNum = Math.floor(Math.random() * app.options.length);
-   const option = app.options[randomNum];
-   alert(option)
- }
-
- let visibility = false;
- const toggleVisibilityHandler = () => {
-   visibility = !visibility
- }
-
-  return (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p> }
-    <p>{app.options.length > 0 ? 'Here are your options': 'No options'}</p>
-    <button disabled={app.options.length === 0} onClick={makeDecisionHandler}>What should i do?</button>
-    <button onClick={removeAllHandler}>Remove all</button>
-    <ol>
-     {
-       app.options.map((option) => {
-         return <li key={option}>{option}</li>
-       })
-     }
-    </ol>
-    <form onSubmit={onFormSubmitHandler}>
-      <input type="text" name='option' />
-      <button>Add Option</button>
-    </form>
-     
-     <h3>Visibility Toggle</h3>
-     <button onClick={toggleVisibilityHandler}>{visibility ? 'Hide details' : 'Show details'}</button>
-
-  </div>
-  );
 }
 
-export default App;
+
+class Action extends Component {
+  render () {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    )
+  }
+}
+
+class Options extends Component {
+  render () {
+    return (
+      <div>
+        Options component here
+      </div>
+    )
+  }
+}
+
+class AddOption extends Component {
+  render () {
+    return (
+      <div>
+        Add Option component here
+      </div>
+    )
+  }
+}
+
+export default Header;

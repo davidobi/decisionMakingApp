3bc14b0a220d673ce6bbd705f9403e2a26e5fa8d
davidobi <dbiggestdave@yahoo.com>
2022-04-15 15:03:28 +0000

    complet coding of the class component with error checks

--- decisionmaker/src/App.js
+++ decisionmaker/src/App.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
 
-class IndecisonApp extends Component {
+class IndecisionApp extends Component {
+  constructor (props) {
+    super (props);
+    this.deleteOptionshandler = this.deleteOptionshandler.bind(this);
+    this.selectDecisionHandler = this.selectDecisionHandler.bind(this);
+    this.addOptionHandler = this.addOptionHandler.bind(this);
+    this.state = {
+      options: []
+    }
+  }
+
+  deleteOptionshandler () {
+    this.setState ((prevState) => {
+      return {
+        options: []
+      };
+    });
+  }
+
+  selectDecisionHandler () {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option);
+  }
+
+  addOptionHandler (option) {
+    if(!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists'
+    }
+    this.setState((prevState)=>{
+      return {
+        options: prevState.options.concat([option])
+      };
+    });
+  }
+
   render() {
     const title = 'Indecision App';
-    const subtitle = 'Put your life ib the hands of a computer';
-    const options = ['Thing one', 'Thing two', 'Thing three'];
+    const subtitle = 'Put your life in the hands of a computer';
+
     return (
       <div>
         <Header title={title} subtitle={subtitle}/>
-        <Action />
-        <Options options={options}/>
-        <AddOption />
+        <Action 
+          hasOptions={this.state.options.length > 0}
+          selectDecisionHandler = {this.selectDecisionHandler} 
+        />
+        <Options 
+          options={this.state.options}
+          deleteOptionshandler = {this.deleteOptionshandler}
+        />
+        <AddOption addOptionHandler={this.addOptionHandler}/>
       </div>
     );
   }
@@ -29,26 +72,24 @@ class Header extends Component {
 
 
 class Action extends Component {
-  selectDecisionHandler () {
-    alert('Clicked me');
-  }
   render () {
     return (
       <div>
-        <button onClick={this.selectDecisionHandler}>What should I do?</button>
+        <button 
+          onClick={this.props.selectDecisionHandler}
+          disabled={!this.props.hasOptions}    
+      >
+          What should I do?</button>
       </div>
     )
   }
 }
 
 class Options extends Component {
-  clearOptionshandler () {
-    alert('all options cleared')
-  }
   render () {
     return (
       <div>
-        <button onClick={this.clearOptionshandler}>Remove all</button>
+        <button onClick={this.props.deleteOptionshandler}>Remove all</button>
         {this.props.options.length}
         {
           this.props.options.map((option) => <Option key={option} optionText={option} /> )
@@ -70,18 +111,30 @@ class Option extends Component {
 }
 
 class AddOption extends Component {
+  constructor (props) {
+    super(props);
+    this.addOptionHandler = this.addOptionHandler.bind(this);
+    this.state = {
+      error: undefined
+    };
+  }
+
   addOptionHandler (e) {
     e.preventDefault();
 
     const option = (e.target.elements.option.value.trim());
-    if (option) {
-      alert(option);
-    }
+    const error = this.props.addOptionHandler(option);
+    this.setState(() => {
+      return { error };
+    });
   }
   render () {
     return (
       <div>
-        <form onSubmit={this.addOptionHandler}>
+        {this.state.error && <p>{this.state.error}</p> }
+        <form 
+          onSubmit={this.addOptionHandler}
+        >
           <input type="text" name='option' />
           <button>Add Option</button>
         </form>
@@ -89,5 +142,5 @@ class AddOption extends Component {
     )
   }
 }
-
-export default IndecisonApp;
+  
+export default IndecisionApp;

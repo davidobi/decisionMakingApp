e13c88396ffbd7f7e43fb4ab32cbedf7a65fa1fc
davidobi <dbiggestdave@yahoo.com>
2022-04-15 07:35:09 +0000

    add input and itenary statement to display options

--- decisionmaker/src/App.js
+++ decisionmaker/src/App.js
@@ -3,12 +3,29 @@ import './App.css';
 function App() {
    const app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of  computer'
+    subtitle: 'Put your life in the hands of  computer',
+    options: ['One', 'Two']
+  };
+
+  const onFormSubmitHandler = (e) => {
+    e.preventDefault(); 
+    console.log('form submitted')
   }
+  
+
   return (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p> }
+    <p>{app.options.length > 0 ? 'Here are your options': 'No options'}</p>
+    <ol>
+      <li>Item One</li>
+      <li>Item Two</li>
+    </ol>
+    <form onSubmit={onFormSubmitHandler}>
+      <input type="text" name='option' />
+      <button>Add Option</button>
+    </form>
   </div>
   );
 }
